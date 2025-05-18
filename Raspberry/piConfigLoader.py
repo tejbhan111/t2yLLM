5a308c74ba250113fa749b2d031a6d3d5ef4b36f
Saga9103 <146200001+Saga9103@users.noreply.github.com>
2025-05-18 16:58:27 +0200

    Add files via upload

---
+++ Raspberry/piConfigLoader.py
@@ -0,0 +1,33 @@
+import yaml
+import os
+from dacite import from_dict
+from dataclasses import dataclass
+
+
+@dataclass
+class PiNetwork:
+    listening_ip: str
+    server_ip: str
+    server_port: int
+    receive_port: int
+    sample_rate: int  # should not be touched
+    channels: int  # should not be touched
+    chunk_size: int
+    output_volume: float
+    buffer_ms: int
+    max_udp_packet: int
+
+
+@dataclass
+class PiConfig:
+    network: PiNetwork
+
+
+class Loader:
+    def __init__(self):
+        file = os.path.join(os.geitcwd(), "PiConfig.yaml")
+        with open(file, "r") as config:
+            self.config = yaml.safe_load(config)
+
+        def load_pi_config(self):
+            return from_dict(data_class=PiConfig, data=self.config["Pi"])
